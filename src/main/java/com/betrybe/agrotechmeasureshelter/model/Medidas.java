c3f2316e1c81204800cf78c6d1b81493485737b8
unamednada <dias.gustavo.dias@gmail.com>
2022-07-28 16:41:23 -0300

    feat: create medidas model

---
+++ src/main/java/com/betrybe/agrotechmeasureshelter/model/Medidas.java
@@ -0,0 +1,69 @@
+package com.betrybe.agrotechmeasureshelter.model;
+
+import java.time.LocalDateTime;
+
+public class Medidas {
+
+  private Integer idIlha;
+
+  private double temperatura;
+
+  private double humidadeSolo;
+
+  private double humidadeAr;
+
+  private LocalDateTime instante;
+
+  public Medidas() {
+
+  }
+
+  public Medidas(Integer idIlha, double temperatura, double humidadeSolo, double humidadeAr,
+      LocalDateTime instante) {
+    this.idIlha = idIlha;
+    this.temperatura = temperatura;
+    this.humidadeSolo = humidadeSolo;
+    this.humidadeAr = humidadeAr;
+    this.instante = instante;
+  }
+
+  public Integer getIdIlha() {
+    return idIlha;
+  }
+
+  public void setIdIlha(Integer idIlha) {
+    this.idIlha = idIlha;
+  }
+
+  public double getTemperatura() {
+    return temperatura;
+  }
+
+  public void setTemperatura(double temperatura) {
+    this.temperatura = temperatura;
+  }
+
+  public double getHumidadeSolo() {
+    return humidadeSolo;
+  }
+
+  public void setHumidadeSolo(double humidadeSolo) {
+    this.humidadeSolo = humidadeSolo;
+  }
+
+  public double getHumidadeAr() {
+    return humidadeAr;
+  }
+
+  public void setHumidadeAr(double humidadeAr) {
+    this.humidadeAr = humidadeAr;
+  }
+
+  public LocalDateTime getInstante() {
+    return instante;
+  }
+
+  public void setInstante(LocalDateTime instante) {
+    this.instante = instante;
+  }
+}
