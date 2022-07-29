5d1845bcf1464e7179804dc8ed799aa4a6386cff
LucasAccurcio <lucas.accurcio@gmail.com>
2022-07-29 14:43:15 -0300

    refactor: Service to use Repository

--- src/main/java/com/betrybe/agrotechmeasureshelter/service/IlhaService.java
+++ src/main/java/com/betrybe/agrotechmeasureshelter/service/IlhaService.java
@@ -1,24 +1,29 @@
 package com.betrybe.agrotechmeasureshelter.service;
 
 import com.betrybe.agrotechmeasureshelter.model.Ilha;
+import com.betrybe.repository.IlhaRepository;
 import org.bson.types.ObjectId;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class IlhaService {
 
+  @Inject
+  IlhaRepository ilhaRepository;
+
   public List<Ilha> list() {
-    List<Ilha> ilhas = Ilha.listAll();
+    List<Ilha> ilhas = ilhaRepository.listAll();
     return ilhas;
   }
 
   public void add(Ilha ilha) {
-    Ilha.persist(ilha);
+    ilhaRepository.persist(ilha);
   }
 
   public Ilha findById(String id) {
-    Ilha ilha = Ilha.findById(new ObjectId(id));
+    Ilha ilha = ilhaRepository.findById(new ObjectId(id));
     // Precisa criar essa classe
     // if (ilha == null) {
     // throw new NotFoundException();
@@ -30,25 +35,25 @@ public class IlhaService {
     Ilha updateIlha = findById(id);
     updateIlha.setName(ilha.getName());
     updateIlha.setStatus(ilha.getStatus());
-    updateIlha.update();
+    ilhaRepository.update(updateIlha);
     return updateIlha;
   }
 
   public void delete(String id) {
     Ilha ilha = findById(id);
-    ilha.delete();
+    ilhaRepository.delete(ilha);
   }
 
   public Ilha search(String name) {
-    return Ilha.findByName(name);
+    return ilhaRepository.findByName(name);
   }
 
   public Long count() {
-    return Ilha.count();
+    return ilhaRepository.count();
   }
 
   public void dropDatabase() {
-    Ilha.deleteAll();
+    ilhaRepository.deleteAll();
   }
 
 }
