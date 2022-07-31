5403c44567ebba5435ffa437e7ce5895c69f4534
unamednada <dias.gustavo.dias@gmail.com>
2022-07-31 13:41:47 -0300

    feat: create service for medidas and add method list

---
+++ src/main/java/com/betrybe/agrotechmeasureshelter/service/MedidasService.java
@@ -0,0 +1,19 @@
+package com.betrybe.agrotechmeasureshelter.service;
+
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import com.betrybe.agrotechmeasureshelter.model.Medidas;
+import com.betrybe.agrotechmeasureshelter.repository.MedidasRepository;
+
+@ApplicationScoped
+public class MedidasService {
+
+  @Inject
+  MedidasRepository medidasRepository;
+
+  public List<Medidas> list() {
+    List<Medidas> medidas = medidasRepository.listAll();
+    return medidas;
+  }
+}
