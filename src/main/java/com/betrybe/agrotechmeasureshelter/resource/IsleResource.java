81300afaf426c06bd42c6e71bf57b3f488b233e2
LucasAccurcio <lucas.accurcio@gmail.com>
2022-08-01 00:55:11 -0300

    feat: add NotFoundException

--- src/main/java/com/betrybe/agrotechmeasureshelter/resource/IsleResource.java
+++ src/main/java/com/betrybe/agrotechmeasureshelter/resource/IsleResource.java
@@ -2,12 +2,14 @@ package com.betrybe.agrotechmeasureshelter.resource;
 
 import java.util.List;
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import com.betrybe.Exception.NotFoundException;
 import com.betrybe.agrotechmeasureshelter.model.Isle;
 import com.betrybe.agrotechmeasureshelter.service.IsleService;
 
@@ -23,26 +25,26 @@ public class IsleResource {
   }
 
   @POST
-  public List<Isle> add(Isle isle) {
+  public List<Isle> add(@Valid Isle isle) throws Exception {
     isleService.add(isle);
     return list();
   }
 
   @GET
   @Path("/{id}")
-  public Isle get(@PathParam("id") String id) {
+  public Isle get(@PathParam("id") String id) throws NotFoundException {
     return isleService.findById(id);
   }
 
   @PUT
   @Path("/{id}")
-  public Isle update(@PathParam("id") String id, Isle isle) {
+  public Isle update(@PathParam("id") String id, Isle isle) throws NotFoundException {
     return isleService.update(id, isle);
   }
 
   @DELETE
   @Path("/{id}")
-  public void delete(@PathParam("id") String id) {
+  public void delete(@PathParam("id") String id) throws NotFoundException {
     isleService.delete(id);
   }
 
