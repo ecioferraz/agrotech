939ed55f2df997bac78c3ce9cad97ed27fbc133b
unamednada <dias.gustavo.dias@gmail.com>
2022-08-01 20:08:59 -0300

    refactor: mock service to use in measurementsresourcetest. fail test

--- src/test/java/com/betrybe/agrotechmeasureshelter/test/MeasurementsResourceTest.java
+++ src/test/java/com/betrybe/agrotechmeasureshelter/test/MeasurementsResourceTest.java
@@ -1,17 +1,36 @@
 package com.betrybe.agrotechmeasureshelter.test;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import com.betrybe.agrotechmeasureshelter.model.Measurements;
+import com.betrybe.agrotechmeasureshelter.service.MeasurementsService;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusMock;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import java.awt.List;
+import java.util.List;
+import java.util.ArrayList;
+import javax.inject.Inject;
 
 @QuarkusTest
 public class MeasurementsResourceTest {
 
+  @Inject
+  MeasurementsService measurementsService;
+
+  @BeforeAll
+  public static void setup() {
+    List<Measurements> mockList = new ArrayList<Measurements>();
+    MeasurementsService mockService = Mockito.mock(MeasurementsService.class);
+    Mockito.when(mockService.list()).thenReturn(mockList);
+    QuarkusMock.installMockForType(mockService, MeasurementsService.class);
+  }
+
   @Test
   public void testSuccesfulGetRequest() {
-    given().when().get("/medidas").then().statusCode(200).body(is(new List()));
+    List<Measurements> emptyList = new ArrayList<Measurements>();
+    given().when().get("/medidas").then().statusCode(200).body(is(emptyList));
   }
 }
