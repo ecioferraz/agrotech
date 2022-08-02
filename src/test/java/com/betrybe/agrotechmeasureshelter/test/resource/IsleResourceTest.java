db3f034ba0de77d710664277a6f57cca55007007
unamednada <dias.gustavo.dias@gmail.com>
2022-08-02 20:25:18 -0300

    tests: test successful getbyid and update

--- src/test/java/com/betrybe/agrotechmeasureshelter/test/resource/IsleResourceTest.java
+++ src/test/java/com/betrybe/agrotechmeasureshelter/test/resource/IsleResourceTest.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import com.betrybe.agrotechmeasureshelter.Exception.NotFoundException;
 import com.betrybe.agrotechmeasureshelter.model.Isle;
 import com.betrybe.agrotechmeasureshelter.service.IsleService;
 import io.quarkus.test.junit.QuarkusMock;
@@ -20,17 +21,32 @@ public class IsleResourceTest {
   IsleService isleService;
 
   @BeforeAll
-  public static void setup() {
+  public static void setup() throws NotFoundException {
     List<Isle> mockList = new ArrayList<Isle>();
     Isle mockIsle = new Isle();
     IsleService mockService = Mockito.mock(IsleService.class);
     Mockito.when(mockService.list()).thenReturn(mockList);
     Mockito.doNothing().when(mockService).add(mockIsle);
+    Mockito.when(mockService.update("blablabla", mockIsle)).thenReturn(mockIsle);
+    Mockito.when(mockService.findById(Mockito.anyString())).thenReturn(mockIsle);
     QuarkusMock.installMockForType(mockService, IsleService.class);
   }
 
   @Test
   public void testSuccesfulGetRequest() {
     given().when().get("/isle").then().statusCode(200).body(containsString("[]"));
+  }
+
+  @Test
+  public void testSuccesfulGetByIdRequest() {
+    given().when().get("/isle/blablabla").then().statusCode(200)
+        .body(containsString("{\"id\":null,\"name\":null,\"status\":null}"));
+  }
+
+  @Test
+  public void testSuccesfulUpdate() {
+    Isle mockIsle = new Isle();
+    given().body(mockIsle).header("Content-Type", "application/json").when().put("/isle/blablabla")
+        .then().statusCode(204);
   }
 }
