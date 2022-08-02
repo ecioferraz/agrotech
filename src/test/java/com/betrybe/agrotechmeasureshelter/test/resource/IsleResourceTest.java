4c3bff713bda2a4e56c4a6b16f9dec16be68192e
unamednada <dias.gustavo.dias@gmail.com>
2022-08-02 18:49:34 -0300

    test: create isleresource success get request

--- src/test/java/com/betrybe/agrotechmeasureshelter/test/resource/IsleResourceTest.java
+++ src/test/java/com/betrybe/agrotechmeasureshelter/test/resource/IsleResourceTest.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import com.betrybe.agrotechmeasureshelter.model.Isle;
 import com.betrybe.agrotechmeasureshelter.service.IsleService;
+import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class IsleResourceTest {
@@ -21,6 +25,12 @@ public class IsleResourceTest {
     Isle mockIsle = new Isle();
     IsleService mockService = Mockito.mock(IsleService.class);
     Mockito.when(mockService.list()).thenReturn(mockList);
+    Mockito.doNothing().when(mockService).add(mockIsle);
+    QuarkusMock.installMockForType(mockService, IsleService.class);
+  }
 
+  @Test
+  public void testSuccesfulGetRequest() {
+    given().when().get("/isle").then().statusCode(200).body(containsString("[]"));
   }
 }
