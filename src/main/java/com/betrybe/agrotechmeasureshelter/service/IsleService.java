c05b690ec35669a5f73d3054572de8a11ccad515
LucasAccurcio <lucas.accurcio@gmail.com>
2022-08-03 00:49:37 -0300

    feat: method to automate the creation of measures

--- src/main/java/com/betrybe/agrotechmeasureshelter/service/IsleService.java
+++ src/main/java/com/betrybe/agrotechmeasureshelter/service/IsleService.java
@@ -4,12 +4,12 @@ import com.betrybe.agrotechmeasureshelter.exception.NotFoundException;
 import com.betrybe.agrotechmeasureshelter.model.Isle;
 import com.betrybe.agrotechmeasureshelter.model.Measurements;
 import com.betrybe.agrotechmeasureshelter.repository.IsleRepository;
+import com.betrybe.agrotechmeasureshelter.util.RandomMeasures;
 import org.bson.types.ObjectId;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import io.quarkus.scheduler.Scheduled;
-import io.quarkus.scheduler.ScheduledExecution;
 
 @ApplicationScoped
 public class IsleService {
@@ -29,18 +29,18 @@ public class IsleService {
     isleRepository.persist(isle);
   }
 
-  @Scheduled(every = "2s") // 5min = PT20M - "PT15M" -- parses as "15 minutes" (where a minute is 60
-                           // seconds)
+  // 1s = "1 segundo" / "PT15M" -- parses as "15 minutes" (where a minute is 60 seconds)
+  @Scheduled(every = "PT5M") // Configurado para 5 minutos.
   public void createAutomatedMeasures() {
     List<Isle> getAllIsle = isleRepository.listAll();
     getAllIsle.stream().forEach(isle -> {
-      String idIsle = isle.getId().toString();
-      double temperature = 20;
-      double soilHumidity = 25;
-      double airHumidity = 45;
-      Measurements newMeasurement =
-          new Measurements(idIsle, temperature, soilHumidity, airHumidity);
-      measurementsService.add(newMeasurement);
+      if (isle.getStatus() == true) {
+        String idIsle = isle.getId().toString();
+        RandomMeasures measure = new RandomMeasures();
+        Measurements newMeasurement = new Measurements(idIsle, measure.getTemperature(),
+            measure.getSoilHumidity(), measure.getAirHumidity());
+        measurementsService.add(newMeasurement);
+      }
     });
   }
 
