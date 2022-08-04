0971e90f92fb18c2e662b07f553fbe74c31ad6f7
LucasAccurcio <lucas.accurcio@gmail.com>
2022-08-03 23:53:51 -0300

    test: findById and dropDatabase

--- src/test/java/com/betrybe/agrotechmeasureshelter/test/service/IsleServiceTest.java
+++ src/test/java/com/betrybe/agrotechmeasureshelter/test/service/IsleServiceTest.java
@@ -1,6 +1,5 @@
 package com.betrybe.agrotechmeasureshelter.test.service;
 
-import static org.mockito.Mockito.times;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -10,10 +9,9 @@ import com.betrybe.agrotechmeasureshelter.repository.IsleRepository;
 import com.betrybe.agrotechmeasureshelter.service.IsleService;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 
@@ -26,57 +24,92 @@ public class IsleServiceTest {
   @InjectMock
   IsleRepository isleRepository;
 
-  @BeforeAll
-  public static void setup() {
-    List<Isle> mockList = new ArrayList<Isle>();
-    Isle southIsle = new Isle("South Isle", true);
-    mockList.add(southIsle);
-    IsleRepository mockRepository = Mockito.mock(IsleRepository.class);
-    Mockito.doNothing().when(mockRepository).persist(Mockito.any(Isle.class));
-    Mockito.when(mockRepository.listAll()).thenReturn(mockList);
-    Mockito.when(mockRepository.findById(new ObjectId("62ead48515906f4f3f529240")))
-        .thenReturn(southIsle);
-    Mockito.when(mockRepository.count()).thenReturn((long) 1);
-    QuarkusMock.installMockForType(mockRepository, IsleRepository.class);
+  private Isle isle;
+
+  @BeforeEach
+  public void setup() {
+    isle = new Isle();
+    isle.setName("South Isle");
+    isle.setStatus(true);
   }
 
   @Test
   public void testListAllIsle() {
-    List<Isle> emptyList = isleService.list();
-    Assertions.assertTrue(emptyList.size() == 0);
+    List<Isle> isleList = new ArrayList<Isle>();
+    isleList.add(isle);
+    Mockito.when(isleRepository.listAll()).thenReturn(isleList);
+
+    Assertions.assertTrue(isleService.list() == isleList);
+
   }
 
   @Test
   public void testCreateIsle() {
-    Isle mockMeasurement = new Isle("Ilha Sul", true);
-    isleService.add(mockMeasurement);
-    Mockito.verify(isleRepository, times(1)).persist(mockMeasurement);
+    Mockito.doNothing().when(isleRepository).persist(Mockito.any(Isle.class));
+
+    List<Isle> isleList = new ArrayList<Isle>();
+    isleList.add(isle);
+
+    Isle newIsle = new Isle("North Island", true);
+    isleList.add(newIsle);
+    isleService.add(newIsle);
+
+    Assertions.assertEquals(2, isleList.size());
   }
 
-  // @Test
-  // public void testFindById() throws NotFoundException {
-  // Isle southIsle = new Isle("South Isle", true);
-  // // Isle findIsle = isleService.findById("62ead48515906f4f3f529240");
-  // Assertions.assertEquals("{\"message\": \"Content not found\"}",
-  // isleRepository.findById(new ObjectId("62ead48515906f4f3f529240")));
-  // }
+  @Test
+  public void testDeleteIsle() throws NotFoundException {
+    List<Isle> isleList = new ArrayList<Isle>();
+    isleList.add(isle);
+
+    Mockito.doNothing().when(isleRepository).delete(Mockito.any(Isle.class));
+
+    Mockito.when(isleRepository.findById(new ObjectId("62ead48515906f4f3f529240")))
+        .thenReturn(isleList.get(0));
+    isleService.delete("62ead48515906f4f3f529240");
+    isleList.remove(0);
+
+    Mockito.when(isleRepository.count()).thenReturn((long) isleList.size());
+    Assertions.assertEquals(0, isleService.count());
+  }
+
+  @Test
+  public void testFindByIdIsle() throws NotFoundException {
+    List<Isle> isleList = new ArrayList<Isle>();
+    isleList.add(isle);
+
+    Mockito.when(isleRepository.findById(new ObjectId("62ead48515906f4f3f529240")))
+        .thenReturn(isleList.get(0));
+
+    Mockito.when(isleRepository.count()).thenReturn((long) isleList.size());
+    Assertions.assertEquals(1, isleService.count());
+  }
 
   @Test
   public void testSearchIsle() {
     Isle newIsle = new Isle("Ilha", true);
     Mockito.when(isleRepository.findByName("Ilha")).thenReturn(newIsle);
-    Assertions.assertEquals(newIsle, isleRepository.findByName("Ilha"));
+    Assertions.assertEquals(newIsle, isleService.search("Ilha"));
   }
 
   @Test
   public void testDropDatabaseIsle() {
+    List<Isle> isleList = new ArrayList<Isle>();
+    isleList.add(isle);
+    Mockito.when(isleRepository.count()).thenReturn((long) isleList.size());
+    Assertions.assertEquals(1, isleService.count());
+
     Mockito.when(isleRepository.deleteAll()).thenReturn(0L);
-    Assertions.assertEquals(0, isleRepository.count());
+    isleService.dropDatabase();
+    isleList.clear();
+    Mockito.when(isleRepository.listAll()).thenReturn(isleList);
+    List<Isle> findIsles = isleService.list();
+    Assertions.assertTrue(findIsles.isEmpty());
   }
 
   @Test
   public void testCountIsle() {
     Mockito.when(isleRepository.count()).thenReturn(23L);
-    Assertions.assertEquals(23, isleRepository.count());
+    Assertions.assertEquals(23, isleService.count());
   }
 }
