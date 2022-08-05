9abef3b687fdef93ea84f4361681742a0d41fada
LucasAccurcio <lucas.accurcio@gmail.com>
2022-08-05 10:07:24 -0300

    stye: IsleResource in restful pattern

--- src/main/java/com/betrybe/agrotechmeasureshelter/resource/IsleResource.java
+++ src/main/java/com/betrybe/agrotechmeasureshelter/resource/IsleResource.java
@@ -3,12 +3,16 @@ package com.betrybe.agrotechmeasureshelter.resource;
 import java.util.List;
 import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import com.betrybe.agrotechmeasureshelter.exception.NotFoundException;
 import com.betrybe.agrotechmeasureshelter.model.Isle;
 import com.betrybe.agrotechmeasureshelter.service.IsleService;
@@ -20,24 +24,30 @@ public class IsleResource {
   IsleService isleService;
 
   @GET
+  @Produces(MediaType.APPLICATION_JSON)
   public List<Isle> list() {
     return isleService.list();
   }
 
   @POST
-  public List<Isle> add(@Valid Isle isle) throws Exception {
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response add(@Valid Isle isle) throws Exception {
     isleService.add(isle);
-    return list();
+    return Response.status(201).build();
   }
 
   @GET
   @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
   public Isle get(@PathParam("id") String id) throws NotFoundException {
     return isleService.findById(id);
   }
 
   @PUT
   @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
   public Isle update(@PathParam("id") String id, Isle isle) throws NotFoundException {
     return isleService.update(id, isle);
   }
@@ -50,12 +60,14 @@ public class IsleResource {
 
   @GET
   @Path("/search/{name}")
+  @Produces(MediaType.APPLICATION_JSON)
   public Isle search(@PathParam("name") String name) {
     return isleService.search(name);
   }
 
   @GET
   @Path("/count")
+  @Produces(MediaType.TEXT_PLAIN)
   public Long count() {
     return isleService.count();
   }
